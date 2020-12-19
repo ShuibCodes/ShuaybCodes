6f7bbe5efa2e0e9610986ad0730ba3ee12848da9
Shuib Abdillahi <shuibabdillahi@MacBook-Pro.local>
2020-12-19 18:15:29 +0000

    made navbar responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,68 @@
-import React from 'react'
-import { Link } from 'gatsby'
-//import './header.module.scss'
-import headerStyles from './Navbar.module.scss'
-import custom from '../pages/custom.scss'
+import React, { useEffect, useState } from "react"
+import { FaBars, FaTimes } from "react-icons/fa"
+import { IconContext } from "react-icons/lib"
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavIcon,
+  NavItem,
+  NavLogo,
+  NavMenu,
+  NavLinks
+} from "../components/NavbarElements"
 
-const Header= () =>{
-    
+const Header = () => {
+  const [click, setClick] = useState(false)
+  const [scroll, setScroll] = useState(false)
 
-    
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
 
-    return(
-       <header className={headerStyles.container}>
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScroll(true)
+    } else {
+      setScroll(false)
+    }
+  }
 
-            <Link to='/' className={headerStyles.title}> 
-              <div className={headerStyles.title}> Title&Logo </div>
-              
-            </Link>     
-            <nav>
-                <ul className={headerStyles.navList}>
-                    <li>
-                    <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
-                    </li>
-                    <li>
-                    <Link  className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                    <Link    className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}to="/about">About</Link>
-                    </li>
-                    <li>
-                    <Link    className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-       </header>
-        
-    )
+  useEffect(() => {
+    changeNav()
+    window.addEventListener("scroll", changeNav)
+  }, [])
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#141414" }}>
+        <Nav active={scroll} click={click}>
+          <NavbarContainer>
+            <NavLogo to="/" onClick={closeMobileMenu}>
+              <NavIcon />
+              Title&Logo
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/"> Home </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/blog" > Blog </NavLinks>
+              </NavItem>
+              <NavItem>
+              <NavLinks to="/about" > About </NavLinks>
+              </NavItem>
+              <NavItem>
+              <NavLinks to="/contact" > Contact </NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  )
 }
 
 export default Header
---
+++ src/components/NavbarElements.js
@@ -0,0 +1,132 @@
+import { Link } from "gatsby"
+import {MdShoppingCart} from 'react-icons/md'
+import styled from "styled-components"
+
+
+export const Nav = styled.nav`
+  background: ${({ active }) =>
+    active
+      ? "#fff"
+      : "linear-gradient(to bottom,  rgba(255,255,255, 0.9) 0%,rgba(255,255,255,0) 100%)"};
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  
+
+  @media screen and (max-width: 960px) {
+    background: ${({ click }) => (click ? "#e0ffff" : "#e0ffff")};
+    transition: 0.8s all ease;
+  }
+`
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  max-width: 1000px;
+  position: sticky;
+  
+
+
+`
+
+export const NavLogo = styled(Link)`
+  color: #141414;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  font-style: italic;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: 800;
+
+`
+
+export const NavIcon = styled(MdShoppingCart)`
+  margin: 0 0.5rem 0 2rem;
+  height: 5rem;
+  width: 3rem;
+  position: relative;
+  left: -0.5em;
+`
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 960px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+
+  @media screen and (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    top: ${({ click }) => (click ? "100%" : "-1000px")};
+    opacity: 1;
+    transition: all 0.25s ease;
+    background: #e0ffff 
+  }
+`
+
+export const NavItem = styled.li`
+  height: 80px;
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+    
+  }
+`
+
+export const NavLinks = styled(Link)`
+@import url('https://fonts.googleapis.com/css2?family=Chelsea+Market&display=swap');
+  color: #141414;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  height: 100%;
+  font-family: 'Chelsea Market',sans-serif;
+  text-decoration: none;
+  font-size: 26px;
+
+
+
+  @media screen and (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+
+    &:hover {
+      color: #ff4040;
+      transition: all 0.3s ease;
+      text-decoration: none;
+    }
+  }
+`
+
