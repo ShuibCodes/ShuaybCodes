676f1380d2e803993a1af9c4c7a17ed2c8e97554
Shuib Abdillahi <abdullahshuib124@gmail.com>
2021-01-02 11:47:34 +0000

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layouts/layout'
 const NotFound = () => {
     return(
         <Layout>
-            <h1>Page not found</h1>
+            <h1>Whoops, Page not found</h1>
             <p><Link to="/" >Head Home</Link></p>
         </Layout>
     )
