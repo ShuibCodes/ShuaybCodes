419a7ec028177c754623ac15baac25f9762f8211
Shuib Abdillahi <shuibabdillahi@MacBook-Pro.local>
2021-01-02 11:40:07 +0000

    update gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,39 @@
 require("dotenv").config({
-
-  path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${process.env.NODE_ENV}`,
+    
+   })
+   module.exports = {
+       siteMetadata: {
+           title: 'Wing Chun Academy',
+           author: 'Shuib Abdillahi'
+       },
+       plugins: [
+           `gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
   
-})
-export const siteMetadata = {
-    title: 'Wing Chun Academy',
-    author: 'Shuib Abdillahi'
-}
-export const plugins = [
-    'gatsby-plugin-react-helmet', `gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
-
-    {
-        resolve: 'gatsby-source-contentful',
-        options: {
-            spaceId: process.env.CONTENTFUL_SPACE_ID,
-            accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-        }
-    },
-    'gatsby-plugin-sass',
-    {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            name: 'src',
-            path: `${__dirname}/src/`,
-            name: `images`,
-            path: `${__dirname}/src/images`,
-        }
-    },
-]
+  
+           'gatsby-plugin-react-helmet',`gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
+  
+           {
+               resolve: 'gatsby-source-contentful',
+              options: {
+                  spaceId: process.env.CONTENTFUL_SPACE_ID,
+                  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+              }
+          },
+          'gatsby-plugin-sass',
+          {
+              resolve: 'gatsby-source-filesystem',
+              options: {
+                  name: 'src',
+                  path: `${__dirname}/src/`,
+                  name: `images`,
+                  path:`${__dirname}/src/images`,
+                  
+              }
+          },
+          
+              
+              
+      ]
+      
+  }
