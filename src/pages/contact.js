bdbd46b2a0f238ec0d4b59ecc9e8982f87c2ac04
shuibcodes <abdullahshuib124@gmail.com>
2021-01-24 17:00:46 +0000

    styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,69 +1,15 @@
 import React, { useState }  from "react"
-import axios from "axios";
+
 import Map from '../pages/Map.js'
 import  FooterLayout from '../components/Layouts/BlogLayout'
-import {Helmet} from 'react-helmet'
+
 import '/src/components/styles/custom.css'
 
 const Contact = () => {
     
-    const [serverState, setServerState] = useState({
-      submitting: false, 
-      status: null
-    });
-    const handleServerResponse = (ok, msg, form) => {
-      setServerState({
-        submitting: false,
-        status: { ok, msg }
-      });
-      if (ok) {
-        form.reset();
-      }
-    };
-    const handleOnSubmit = e => {
-      e.preventDefault();
-      const form = e.target;
-      setServerState({ submitting: true });
-      axios({
-        method: "post",
-        url: "https://getform.io/f/a5bd7840-377b-4857-934e-b058c13d4aad",
-        data: new FormData(form)
-      })
-        .then(r => {
-          handleServerResponse(true, "Thanks!", form);
-        })
-        .catch(r => {
-          handleServerResponse(false, r.response.data.error, form);
-        });
-    };
-    return (
-        <FooterLayout>
-        <Helmet title="Contact" />
-    <div className="background">
-        <div className="form">
-            <div className=" form col-md-8 mt-5">
-                <form action="https://getform.io/f/a5bd7840-377b-4857-934e-b058c13d4aad" onSubmit={handleOnSubmit}>
-                <div className="form-group">
-                    <label for="exampleInputEmail" required="required">Email address <input type="email" name="email" className="form-control"  aria-describedby="emailHelp" placeholder="Enter email" required="required"/></label>
-                    <label for="exampleInputName" >Name  <input type="text" name="name"  className="form-control" id="exampleInputName" placeholder="Enter your name" required="required"/> </label>
-                    <label for="exampleInputName" >Message <input type="text" name="name"  className="form-control" placeholder="Type Your Message" id="messageBox" /> </label>
-                </div> 
-              
-                    
-                  
-            
-                
-                <button type="submit" className="btn btn-primary"  disabled={serverState.submitting}>
-                    Submit
-                </button>
-                {serverState.status && (
-                    <p className={!serverState.status.ok ? "errorMsg" : ""}>
-                    {serverState.status.msg}
-                    </p>
-                )}
-                </form>
-            </div>
-            <div className="contact-container">
+   return(
+     <FooterLayout>
+         <div className="contact-container">
               <div className="contact-details">
                   <p> <span className="prompt" >Address: </span>200 Smiths Close , London, N22 5FL </p>
                     <p><span className="prompt" >Telephone: </span>0208115563 </p>
@@ -73,14 +19,10 @@ const Contact = () => {
                     </span>P.O.Box 124, Lords Lane 211b </p>
               </div>
             </div>
-          </div>  
-    </div>
-   
-        <Map />
-      
-      </FooterLayout>
-     
-    );
-  };
+            <Map />
+     </FooterLayout>
+   )
+           
+}
   
   export default Contact;
