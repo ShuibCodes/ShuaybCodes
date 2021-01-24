ce6c60584f32788d623bfb5ced796aa16d969d1a
shuibcodes <abdullahshuib124@gmail.com>
2021-01-24 15:39:36 +0000

    styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -44,9 +44,9 @@ const Contact = () => {
             <div className=" form col-md-8 mt-5">
                 <form action="https://getform.io/f/a5bd7840-377b-4857-934e-b058c13d4aad" onSubmit={handleOnSubmit}>
                 <div className="form-group">
-                    <label for="exampleInputEmail1" required="required">Email address <input type="email" name="email" className="form-control"  aria-describedby="emailHelp" placeholder="Enter email" required="required"/></label>
+                    <label for="exampleInputEmail" required="required">Email address <input type="email" name="email" className="form-control"  aria-describedby="emailHelp" placeholder="Enter email" required="required"/></label>
                     <label for="exampleInputName" >Name  <input type="text" name="name"  className="form-control" id="exampleInputName" placeholder="Enter your name" required="required"/> </label>
-                    <label for="exampleInputName" >Message <input type="text" name="name"  className="form-control" id="exampleInputName" placeholder="Type Your Message" id="messageBox" /> </label>
+                    <label for="exampleInputName" >Message <input type="text" name="name"  className="form-control" placeholder="Type Your Message" id="messageBox" /> </label>
                 </div> 
               
                     
