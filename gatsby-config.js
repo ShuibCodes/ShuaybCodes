264f5d3cfebab2f5a95cd56ce87a9ec49c8937d8
shuibcodes <abdullahshuib124@gmail.com>
2021-08-02 21:10:27 +0100

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,40 @@ require("dotenv").config({
     path: `.env.${process.env.NODE_ENV}`,
     
    })
+
+
    module.exports = {
        siteMetadata: {
            title: 'Wing Chun Academy',
            author: 'Shuib Abdillahi'
        },
        plugins: [
+            
+
+
+
+
            `gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
   
             
            'gatsby-plugin-react-helmet',`gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
   
-           {
+             {
                resolve: 'gatsby-source-contentful',
+             
               options: {
                   spaceId: process.env.CONTENTFUL_SPACE_ID,
                   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-              }
+              },
+             
+             
+          },
+
+          {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+              trackingId: process.env.GA_TRACKING_ID
+            }
           },
           'gatsby-plugin-sass',
           {
