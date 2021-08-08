38bb5fb86f5c9d433b60269e22da780f945e7c59
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2021-08-08 14:35:14 +0100

    Update index.js
    typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const Index = () =>{
 
         <Layout>
           <div className="about-me" >
-            <p className="about-text" > Hi, I'm Shuayb 👋,   I'm a self-taught Developer  currrently working for a Sports Nutrion Company in the UK .
+            <p className="about-text" > Hi, I'm Shuayb 👋,   I'm a self-taught Developer  currrently working for a Sports Nutrition Company in the UK .
             My Journey into tech started on the Summer of 2020 and I'd love to have you along for the ride. I'm currrently working on Data visulation and Web Development using D3.js, React, GraphQL and Gatsby.  My personal projects include and NBA Stats App and various up-comming projects (github on my portfolio) </p>
           </div>
           
