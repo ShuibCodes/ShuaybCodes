85262b579b645cf7fca3e491ddf772a9391d8f5a
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2021-08-20 11:06:44 +0100

    new deply

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,77 +8,28 @@ import '../components/NewStyles/BlogCard.css'
 import '../components/NewStyles/BlogStyles.css'
 import bars from '../images/bars.jpeg'
 const BlogPage = () => {
-
-   // sorting feature:
-
-    // when button is clicked, sort tags into alphabetical order based on slug
-
     const data = useStaticQuery(graphql`
         query {
-
             allContentfulLongPost(sort: {fields: updatedAt, order: DESC}) {
                 edges {
                   node {
                     title
                     Slug
                     updatedAt(formatString:"MMMM Do, YYYY")
-
-            allContentfulBlogPost(sort: {fields: publishedDate, order: DESC}) {
-                edges {
-                  node {
-                    title
-                    slug
-                    publishedDate(formatString: "MMMM Do, YYYY")
-                  }
-                }
-                nodes {
-                  image {
-                    file {
-                      url
-                    }
-
                   }
                 }
               }
         }
     `)
-console.log(data.allContentfulLongPost.edges.map((e) => e.node.title).sort())
-  
     // https://www.youtube.com/watch?v=8t0vNu2fCCM&t=12856s
-
-
-
-
-    
     return (
         <FooterLayout >
         <Helmet title="Blog" />
-
             <div className="blogs-group"  >   
   
              <ol >
                 {data.allContentfulLongPost.edges.map((edge) => {
                     return (
-
-            <div className={blogStyles.background} >   
-              
-             <ol className={blogStyles.posts}>
-             <h2 >Posts</h2>
-                {data.allContentfulBlogPost.edges.map((edge) => {
-                    return (
-                     
-                 
-                        <li className={blogStyles.post}>
-                            <Link to={`/blog/${edge.node.slug}`}>
-                                <div className={blogStyles.text}>
-                
-                                    <h2 style={{fontSize:"25px", position:"relative", top:"10px"}}  className="title" >{edge.node.title}</h2>
-                                    <p className={blogStyles.para} >{edge.node.publishedDate}</p>
-                                   
-                                </div>
-                            </Link> 
-                        </li>
-
                        
                         <div class="container-cards">
 
@@ -109,7 +60,7 @@ console.log(data.allContentfulLongPost.edges.map((e) => e.node.title).sort())
 
                        
                     )
-                }).sort()}
+                })}
             </ol>
             </div>
              
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+  
 import React, {useState} from "react"
 
 import Head from '../components/head'
@@ -14,24 +15,6 @@ const Index = () =>{
   return(
   <>
       <Head title="Home " />
-
-        {/* <HeroSection {...homeObjOne} />
-        <MidSection/>
-        <ImgSection/>
-        */}
-
-        <Layout>
-          <div className="about-me" >
-            <p className="about-text" > Hi, I'm Shuayb 👋,   I'm a self-taught Developer  currrently working for a Sports Nutrition Company in the UK .
-            My Journey into tech started on the Summer of 2020 and I'd love to have you along for the ride. I'm currrently working on Data visulation and Web Development using D3.js, React, GraphQL and Gatsby.  My personal projects include and NBA Stats App and various up-comming projects (github on my portfolio) </p>
-          </div>
-          
-        <Blog/>
-        </Layout>
-
-        
-        
-
       
         <Home/>
            
